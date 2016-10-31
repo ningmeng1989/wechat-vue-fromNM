aceac1721bfc2e2e26fda2b585b4a641d39aabf5
ningmeng1989 <380215444@qq.com>
2016-11-01 00:03:32 +0800

    增加了4个页面

---
+++ src/views/404.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="_full_router component-xxx">
+        <div class="_full_inner">
+            <top-handel
+                :back-text="topModel.backText"
+                :cur-text="topModel.curText"
+                :decline="decline"
+                >
+            </top-handel>
+            <div class="_cover-content _effect"
+                :class="{'_effect--30':decline}"
+                >
+                <div>
+                    <div v-link="{path:'/'}">
+                        返回首页
+                    </div>
+                </div>
+            </div>
+            <!--router-->
+            <router-view transition="cover"></router-view>
+        </div>
+    </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import topHandle from 'topHandle';
+export default{
+    vuex:{
+        getters:{
+
+        },
+        actions:{
+
+        }
+    },
+    route:{
+        activate({from,to,next}) {
+            next()
+        },
+        deactivate({from,to,next}){
+            next()
+        }
+    },
+    data(){
+        return{
+            decline:false,
+            topModel:{
+                backText:'返回',
+                curText:'404',
+                nextPath:{path:''},
+                nextIcon:''
+            }
+        }
+    },
+    methods:{
+
+    },
+    events:{
+        'route-pipe'(_decline){
+            this.decline=_decline;
+            this.$parent.$emit('route-pipe',_decline);
+        }
+    },
+    created(){
+
+    },
+    ready(){
+
+    },
+    components:{
+        topHandle
+    }
+}
+
+
+</script>
